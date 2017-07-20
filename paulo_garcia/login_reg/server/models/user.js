3f944c1c15809d497cf008995b15db812cde895a
pdg0789 <pdg0789@gmail.com>
2017-07-20 14:05:44 -0500

    i hpe this works

--- paulo_garcia/login_reg/server/models/user.js
+++ paulo_garcia/login_reg/server/models/user.js
@@ -2,16 +2,31 @@ let mongoose = require('mongoose');
 let bcrypt=require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
-  name:{
+  first_name:{
     type: String,
     //validations
-    required: [true, 'Name cannot be blank']
+    required: [true, 'First name cannot be blank']
+  },
+  last_name:{
+    type: String,
+    //validations
+    required: [true, 'Last name cannot be blank']
   },
   email:{
     type: String,
+    required: [true, 'Email cannot be blank']
+  },
+  birthday:{
+    type: Date,
+    required: [true, 'Birthday cannot be blank']
   },
   password:{
     type: String,
+    required: [true, 'Password cannot be blank']
+  },
+  password_conf:{
+    type: String,
+    required: [true, 'Password confirmation cannot be blank']
   }
 }, {timestamps: true});
 
@@ -19,5 +34,9 @@ UserSchema.pre('save', function(next){
   this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10));
   next();
 })
+
+UserSchema.methods.authenticate= function(password){
+  return bcrypt.compareSync(password, this.password)
+}
 
 mongoose.model('User', UserSchema);
