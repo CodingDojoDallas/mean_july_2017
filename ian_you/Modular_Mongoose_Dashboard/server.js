04c89302410f97db201487b5124432b6456a6997
Ian You <jungyou87@gmail.com>
2017-07-24 19:31:21 -0500

    Modular Mongoose Dashboard

---
+++ ian_you/Modular_Mongoose_Dashboard/server.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let bp = require('body-parser');
+let mongoose = require('mongoose');
+
+let app = express();
+
+app.use(express.static(__dirname+'/static') );
+app.use(bp.urlencoded({ extended: true}));
+
+app.set('views', __dirname+'/views/');
+app.set('view engine' , 'ejs');
+
+
+
+require('./server/config/mongoose.js')
+
+// Routes
+var routes_setter = require('./server/config/routes.js');
+routes_setter(app);
+
+
+app.listen(8000, function(){
+  console.log("Listening on port 8000...");
+});
